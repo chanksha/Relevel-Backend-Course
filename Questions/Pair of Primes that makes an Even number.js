9b416dbe28465114daf6c8c7e97d6b600beea0ae
Chanksha Lohiya <chanksha98@gmail.com>
2022-07-06 16:31:10 +0530

    Few comments added

--- Questions/Pair of Primes that makes an Even number.js
+++ Questions/Pair of Primes that makes an Even number.js
@@ -1,4 +1,10 @@
-// functions in brief
+// Problem Statement:
+// Given a number A which is an even integer, you need to print two prime numbers whose sum will be
+// equal to A.
+// Constraint: A>2
+// Input - 4
+// Output - 2,2
+
 
 
 //Function to determine whether a number is prime or not
@@ -16,11 +22,11 @@ function isPrime(n) {
 
 //main function to determine the pair of primes 
 function primePair(A) {
-    if(A % 2 === 0) {                                            //checking if given number is even or not.
-        let a = 2, b = 0;                                        //Initaialise the variables to denote the pair.
-        while(a <= A) {                                          //Using while loop till the value of A.
+    if(A % 2 === 0) {   //checking if given number is even or not.
+        let a = 2, b = 0;     //Initaialise the variables to denote the pair.
+        while(a <= A) {   //Using while loop till the value of A.
             b = A - a;                                           
-            if(isPrime(b) == true && isPrime(a) == true) {       //if both a and b are prime then print them.
+            if(isPrime(b) == true && isPrime(a) == true) {   //if both a and b are prime then print them.
                 console.log(a,b);
             }
             a += 1
